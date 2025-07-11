ddd36cf2573ea7e3ec49650b460fc85b63766638
horry <horry@pop-os.localdomain>
2025-07-12 00:39:36 +0200

    added export script

---
+++ export_to_itch.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+project_name=""
+
+godot --export-release "Web" ./exports/web/index.html
+zip -r ./exports/web.zip ./exports/web
+butler push ./exports/web.zip  horryportier/{$project_name}:html5
+
+if [[ $1 == "web" ]]; then
+	exit 0
+fi
+
+godot --export-release "Linux" ./exports/linux/{$project_name}.x86_64
+zip -r ./exports/{$project_name}_linux.zip ./exports/linux/
+butler push ./exports/{$project_name}_linux.zip  horryportier/{$project_name}:linux
+
+godot --export-release "Windows Desktop" ./exports/windows/{$project_name}.exe
+zip -r ./exports/{$project_name}_windows.zip ./exports/windows/
+butler push ./exports/{$project_name}_windows.zip  horryportier/{$project_name}:win
