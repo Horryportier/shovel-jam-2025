19ff786719c53d9b64702ec90707af4f34eea44c
horry <horry@pop-os.localdomain>
2025-07-12 20:03:05 +0200

    added dev level

--- scenes/root/root.gd
+++ scenes/root/root.gd
@@ -1,11 +1,17 @@
 extends Node
 
-
 @export var ldtk_world: PackedScene
-
+@export var dev_world: PackedScene
+@export var is_dev: bool = false
 @onready var world: Node2D = %World
 
 func _ready() -> void:
-	var levels: =  ldtk_world.instantiate()
-	levels.add_to_group("LdtkWorld")
-	world.add_child(levels)
+	if is_dev:
+		push_warning("game is in the dev mode if its suposed to be relese then you fucked up otherwise proceed")
+		var levels: =  dev_world.instantiate()
+		levels.add_to_group("Levels")
+		world.add_child(levels)
+	else: 
+		var levels: =  ldtk_world.instantiate()
+		levels.add_to_group("Levels")
+		world.add_child(levels)
